a966fe53bb97e99b90e3ebf711a441fe1e2dce25
Harihar <hari.harihar20@gmail.com>
2023-02-02 16:08:35 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'register-user-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'registeration-user-form';
+}
