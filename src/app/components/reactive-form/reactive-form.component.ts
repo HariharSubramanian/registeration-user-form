6d70a6bd76d4754cf10682db59e7174f2e8c7f33
Harihar <hari.harihar20@gmail.com>
2023-02-03 23:16:06 +0530

    reactive-form-registration-of-user

---
+++ src/app/components/reactive-form/reactive-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'register-user-reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrls: ['./reactive-form.component.css']
+})
+export class ReactiveFormComponent {
+
+}
