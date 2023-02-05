5c4d79e327d0a1bd53882a35b014ddb892be3d08
Harihar <hari.harihar20@gmail.com>
2023-02-05 22:51:07 +0530

    form component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateFormComponent } from './components/template-form/template-form.component';
 import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent,
     ReactiveFormComponent
   ],
   imports: [
